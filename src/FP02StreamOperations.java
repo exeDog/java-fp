e013512fa0c9347491e49e5838c1bf7c05bd4874
Priyank Rege <priyank.rege@bigcommerce.com>
2023-12-16 15:58:01 -0600

    Functional playground

--- src/FP02StreamOperations.java
+++ src/FP02StreamOperations.java
@@ -4,7 +4,7 @@ public class FP02StreamOperations {
 
     public static void main(String[] args) {
         List<Integer> nums = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-
+        nums.forEach(System.out::println);
 
     }
 }
